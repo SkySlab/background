e891fdd3f2be6110ea50cffeceb9cbd56fedc69b
SkySlab <baileys2611@gmail.com>
2018-07-08 16:36:56 +1000

    Update motionvid.py

--- motionvid.py
+++ motionvid.py
@@ -1,4 +1,4 @@
-import smtplib, os, glob, time
+import smtplib, os, glob, time, datetime, urllib2, json, httplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
@@ -8,32 +8,42 @@ from email import encoders
 
 time.sleep(5) # wait for 5 seconds to finish video encoding
 
+# read and store our secret keys
 with open('/home/pi/background/config.json') as config_file:
         user_config = json.load(config_file)
         wu_api = user_config['weather_underground_api']
         ts_api = user_config['thingspeak_api']
         google_key = user_config['google_application_key']
+        location = user_config['location'] + ".json"
+        fromaddr = user_config['fromaddr']
+        toaddr = user_config['toaddr']
+        google_login = user_config['google_login']
+
+# get the ambient temperature
+def getAmbient():
+        f = urllib2.urlopen('http://api.wunderground.com/api/' + wu_api + '/conditions/forecast/q/' + location)
+        json_string = f.read()
+        parsed_json = json.loads(json_string)
+        currentTemp_c = parsed_json['current_observation']['temp_c']
+        return(currentTemp_c)
+        f.close()
 
 # define message parameters and create the container
 
-fromaddr = "baileys2611@gmail.com"
-toaddr = "baileys2611@gmail.com"
 msg = MIMEMultipart()
-msg['From'] = 'baileys2611@gmail.com'
-msg['To'] = 'baileys2611@gmail.com'
+msg['From'] = fromaddr
+msg['To'] = toaddr
 msg['Subject'] = "Motion Cam Activated"
 body = "Video of Motion Detected"
 msg.attach(MIMEText(body, 'plain'))
 
 # get the picture and video path and name
-
 last_photo_taken = sorted(glob.glob("/var/lib/motion/*.jpg"),key=os.path.getmtime)[-1]
 photo_name = os.path.basename(last_photo_taken)
 last_video_taken = sorted(glob.glob("/var/lib/motion/*.avi"),key=os.path.getmtime)[-1]
 video_name = os.path.basename(last_video_taken)
 
 # attach the video to the email
-
 attachment = open(last_video_taken, "rb")
 part = MIMEBase('application', "octet-steam")
 part.set_payload((attachment).read())
@@ -53,7 +63,7 @@ msg.attach(img)
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('baileys2611@gmail.com', google_key)
+server.login(google_login, google_key)
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
 server.quit()
